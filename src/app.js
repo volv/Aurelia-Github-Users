410b135442acf143b36943c3f75f780e4c7366d0
Dave Cohen <dcohen18@gmail.com>
2017-08-28 16:00:00 -0500

    router

--- src/app.js
+++ src/app.js
@@ -2,10 +2,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
-      { route: 'github-app',  name: 'github-app', moduleId: 'github-app', nav: true, title: 'Search GitHub User' }
+      { route: ['', 'github-app'],  name: 'github-app', moduleId: 'github-app', nav: true, title: 'Search GitHub User' }
     ]);
 
     this.router = router;
